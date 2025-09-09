6c1bcdebd0a417e6874cc183f14c71ad2d5309f7
gopisai123 <gopijanga2006@gmail.com>
2025-09-09 07:59:22 +0530

    commit

--- codecrew/lib/supabaseClient.ts
+++ codecrew/lib/supabaseClient.ts
@@ -7,7 +7,15 @@
 
 
 
+// import { createClient } from '@supabase/supabase-js';
+// const supabaseUrl = 'https://cxtfirigocayekwlcfye.supabase.co';
+// const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN4dGZpcmlnb2NheWVrd2xjZnllIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTczNDM5NDEsImV4cCI6MjA3MjkxOTk0MX0.W1gMLyixc4HLAeKkvlmb5dhEfQbFfWSdcvazyhkijsI';
+// export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
+
 import { createClient } from '@supabase/supabase-js';
+
 const supabaseUrl = 'https://cxtfirigocayekwlcfye.supabase.co';
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN4dGZpcmlnb2NheWVrd2xjZnllIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTczNDM5NDEsImV4cCI6MjA3MjkxOTk0MX0.W1gMLyixc4HLAeKkvlmb5dhEfQbFfWSdcvazyhkijsI';
+
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
